f0d2fb3329b56867f147603325641b13d97a41fa
Miguel Pardo <migsub77@gmail.com>
2018-10-14 15:50:46 -0700

    fixed style for buttons

--- src/components/global/Button/button.js
+++ src/components/global/Button/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
-import './button.scss';
+import '../../../_styles/components/global/button.css';
 
 const Button = (props) => {
   const { text, type, large, isExternal, url, isActive, callback } = props;
